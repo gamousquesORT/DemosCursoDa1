d7bf1e728d455b29c718e0c0a92ff3fa4ff86d2a
gamousquesORT <mousques@ort.edu.uy>
2025-03-21 16:42:17 -0300

    added using this delegation to estabish relationships

---
+++ UML/ClasesRelaciones/CountryCapitalRelationship/src/Capital.cs
@@ -0,0 +1,34 @@
+﻿namespace CountryCapitalRelationship;
+
+public class Capital
+{
+    private Country? _country;
+
+    public Capital(string cityName)
+    {
+        this.Name = cityName;
+    }
+
+    public string Name { get; }
+
+    public string CountryName
+    {
+        get
+        {
+            if (_country != null) 
+                return _country.Name; 
+            throw new NullReferenceException();
+        }
+    }
+
+    public Country Country
+    {
+        get
+        {
+            if (_country != null) 
+                return _country; 
+            throw new NullReferenceException();
+        }
+        set => _country = value;
+    }
+}
---
+++ UML/ClasesRelaciones/CountryCapitalRelationship/src/Country.cs
@@ -0,0 +1,24 @@
+﻿namespace CountryCapitalRelationship;
+
+public class Country
+{
+    private Capital? _capital;
+    public Country(string countryName)
+    {
+        Name = countryName;
+    }
+
+    public Capital Capital
+    {
+        get
+        {
+            if (_capital != null) return _capital; else throw new NullReferenceException();
+        }
+        set => _capital = value;
+    }
+
+    public string Name { get; }
+
+    public string CapitalName => Capital.Name ?? throw new NullReferenceException();
+    
+}
