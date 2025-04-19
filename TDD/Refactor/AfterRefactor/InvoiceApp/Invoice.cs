b7d94a4c4941236464f9d394ce2ac69e584548a7
gamousquesORT <mousques@ort.edu.uy>
2025-04-19 11:55:48 -0300

    Changed Magic Number

--- TDD/Refactor/AfterRefactor/InvoiceApp/Invoice.cs
+++ TDD/Refactor/AfterRefactor/InvoiceApp/Invoice.cs
@@ -2,12 +2,13 @@
 
 public class Invoice                         // → Rename Class
 {
+    private const double DEFAULT_TAX_RATE = 1.2; 
     public double Calculate(double price, double quantity)
     {
         if (price < 0 || quantity < 0) // → Extract Method
         {
             throw new ArgumentException("Price and quantity must be greater than zero.");
         }
-        return price * quantity * 1.2;       // → Extract Field (magic number)
+        return price * quantity * DEFAULT_TAX_RATE;       // → Extract Field (magic number)
     }
 }
