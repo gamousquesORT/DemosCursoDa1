1622f0a12d842bba0768ac0de32c6b5990786ffa
gamousquesORT <mousques@ort.edu.uy>
2025-04-19 12:02:06 -0300

    Encapsulate Fields quantity and price

--- TDD/Refactor/AfterRefactor/InvoiceApp/Invoice.cs
+++ TDD/Refactor/AfterRefactor/InvoiceApp/Invoice.cs
@@ -4,15 +4,28 @@ public class Invoice                         // → Rename Class
 {
     private double _price;
     private double _quantity;
-    private const double DEFAULT_TAX_RATE = 1.2; 
+    private const double DEFAULT_TAX_RATE = 1.2;
+
+    public double Price
+    {
+        get => _price;
+        set => _price = value;
+    }
+
+    public double Quantity
+    {
+        get => _quantity;
+        set => _quantity = value;
+    }
+
     public double Calculate(double price, double quantity)
     {
-        _price = price;
-        _quantity = quantity;
-        if (_price < 0 || _quantity < 0) // → Extract Method
+        Price = price;
+        Quantity = quantity;
+        if (Price < 0 || Quantity < 0) // → Extract Method
         {
             throw new ArgumentException("Price and quantity must be greater than zero.");
         }
-        return _price * _quantity * DEFAULT_TAX_RATE;       // → 1. Extract Field (magic number)
+        return Price * Quantity * DEFAULT_TAX_RATE;       // → 1. Extract Field (magic number)
     }
 }
