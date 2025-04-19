972ab50d0b1b532d9e14268b32a164f7cec896e3
gamousquesORT <mousques@ort.edu.uy>
2025-04-19 11:52:30 -0300

    Berfore refactoring

---
+++ TDD/Refactor/AfterRefactor/InvoiceApp/Invoice.cs
@@ -0,0 +1,13 @@
+﻿namespace InvoiceApp;
+
+public class Invoice                         // → Rename Class
+{
+    public double Calculate(double price, double quantity)
+    {
+        if (price < 0 || quantity < 0) // → Extract Method
+        {
+            throw new ArgumentException("Price and quantity must be greater than zero.");
+        }
+        return price * quantity * 1.2;       // → Extract Field (magic number)
+    }
+}
