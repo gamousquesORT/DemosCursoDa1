21c609051a26fbdf5f7816af75abdf9c008cc31f
Gaston Mousques <gastonmousques@Gastons-MacBook-Pro-2.local>
2025-10-02 12:03:17 -0300

    improved example of refactoring if to polimorphism

--- CleanCode/RefactorIfs/IfsBeforeRefactor/PaymentProcessor.cs
+++ CleanCode/RefactorIfs/IfsBeforeRefactor/PaymentProcessor.cs
@@ -4,21 +4,44 @@ public class PaymentProcessor
 {
     public void ProcessPayment(object payment, decimal amount)
     {
+        //esto hay que mejorarlo,
+        //tampoco es bueno usar el downcasting
+        
         if (payment is CreditCard)
         {
-            Console.WriteLine($"Processing credit card payment: ${amount}");
+            ((CreditCard) payment).ProcessCreditCard(amount);
         }
         else if (payment is PayPal)
         {
-            Console.WriteLine($"Processing PayPal payment: ${amount}");
+            ((PayPal) payment).ProcessPayPal(amount);
         }
         else if (payment is BankTransfer)
         {
-            Console.WriteLine($"Processing bank transfer: ${amount}");
+            ((BankTransfer) payment).ProcessBankTransfer(amount);
         }
     }
 }
 
-public class CreditCard { }
-public class PayPal { }
-public class BankTransfer { }
+public class CreditCard
+{
+    public void ProcessCreditCard(decimal amount)
+    {
+        Console.WriteLine($"Processing credit card payment: ${amount}");
+    }
+}
+
+public class PayPal
+{
+    public void ProcessPayPal(decimal amount)
+    {
+        Console.WriteLine($"Processing PayPal payment: ${amount}");
+    }   
+}
+
+public class BankTransfer
+{
+    public void ProcessBankTransfer(decimal amount)
+    {
+        Console.WriteLine($"Processing bank transfer: ${amount}");
+    }  
+}
